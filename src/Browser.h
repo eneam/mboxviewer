40fa306003d3af7a816d88b9b93dda6c36a03503
zigm <zbigniew.minciel@gmail.com>
2019-11-05 18:10:13 -0600

    Updated license realted notes.

--- src/Browser.h
+++ src/Browser.h
@@ -9,11 +9,9 @@
 //
 //  Copyright(C) 2019  Enea Mansutti, Zbigniew Minciel
 //
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Library General Public
-//  License as published by the Free Software Foundation; 
-//  as version 2 of the License.
-//  either version 2 of the License, or (at your option) any later version.
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the version 3 of GNU Affero General Public License
+//  as published by the Free Software Foundation; 
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
